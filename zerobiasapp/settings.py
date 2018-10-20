4f7d7969334654de1c67f448f93301339bd91cdf
Gabrielle Trotter <gtrotter@bulogics.com>
2018-10-20 13:31:52 -0700

    Add html template for page

--- zerobiasapp/settings.py
+++ zerobiasapp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'zerobias.apps.ZerobiasConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
