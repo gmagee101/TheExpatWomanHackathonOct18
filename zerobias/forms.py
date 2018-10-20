e3a1b340cc2e6da3288160c0ddfbee2b6d0332a4
Gabrielle Trotter <gtrotter@bulogics.com>
2018-10-20 15:33:29 -0700

    Create basic web form

---
+++ zerobias/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+
+class JobDescForm(forms.Form):
+    job_desc = forms.CharField(label="Job Description", widget=forms.Textarea)
--- zerobias/views.py
+++ zerobias/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .forms import JobDescForm
+
 def index(request):
-    return render(request, 'zerobias/index.html', {})
+    if request.method == 'POST':
+        form = JobDescForm(request.POST)
+        if form.is_valid():
+            # TODO: process data!
+            return render(request, 'zerobias/analyze.html', {'input_text': form.cleaned_data['job_desc'].replace("\n", "<br>")})
+    else: # this was a 'GET' request or other type
+        form = JobDescForm()
+    return render(request, 'zerobias/index.html', {'form': form})
