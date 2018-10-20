b7a668a02a40ef28cdefefaf006d5ad45f6878c5
Paxton Kammermeier <paxtonkammermeier@Paxtons-MacBook-Air.local>
2018-10-20 15:25:18 -0700

    created models and added to admin consule

--- zerobias/admin.py
+++ zerobias/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Highlight
+from .models import Reason
+
+admin.site.register(Highlight)
+admin.site.register(Reason)
--- zerobias/models.py
+++ zerobias/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Reason(models.Model):
+    reason = models.CharField(max_length=1000)
+    def __str__(self):
+        return self.reason
+
+class Highlight(models.Model):
+    key = models.CharField(max_length=200)
+    suggestions = models.CharField(max_length=1000)
+    reason = models.ForeignKey(Reason, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.key
+    def __str__(self):
+        return self.suggestions
